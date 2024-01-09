0c4c5b905e2c95133a3057a2a01f54d2d0556b49
MattManzi <matty.vivi.16@gmail.com>
2024-01-09 15:32:59 +0100

    Correzione versione Solidity e versione openZeppelin

--- contracts/Sulboundtoken.sol
+++ contracts/Sulboundtoken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.7.2/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.7.2/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.7.2/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts@4.7.2/access/Ownable.sol";
-import "@openzeppelin/contracts@4.7.2/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MeowBurn is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
