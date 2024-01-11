0b408b0616a6b0a70dd314c95c28aa450d6971cd
MattManzi <matty.vivi.16@gmail.com>
2024-01-11 19:17:33 +0100

    Configurazione e creazione file json dell'SBT

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.4",
+
+    networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+    }
+  }
 };
